f876db1b18d7bd46d57dca05536fd7408de27412
Frederick Johannson <f.johannson@hotmail.de>
2022-12-08 09:32:20 +0100

    cosmetic changes to the map

--- app.py
+++ app.py
@@ -32,20 +32,20 @@ add_bg_from_url()
 
 #with tab1:
 
+#'''
+# 🚲 Oslo City Bike - Maintenance Forecast 🚲
+#'''
 original_title = '<p style="font-weight: bold; font-family:Source Sans Pro; color:Black; font-size: 42px;">🚲 Oslo City Bike - Maintenance Forecast 🚲</p>'
 st.markdown(original_title, unsafe_allow_html=True)
-sub_title = '<p style="color:Black; font-size: 25px;">Predict bicycle station usage</p>'
+sub_title = '<p style="color:Black;">Predict bicycle station usage</p>'
 st.markdown(sub_title, unsafe_allow_html=True)
 
 today = datetime.today()
 td = timedelta(days=5)
 max_day = today + td
 
-#select_title = '<p style="color:Black; font-size: 30px;">Please choose date</p>'
-#st.markdown(select_title, unsafe_allow_html=True)
-
 with st.form(key='params_for_api'):
-    date_to_predict = st.date_input('Please enter a date', value=datetime(2022,12,2), label_visibility='hidden')# min_value=today, max_value=max_day)
+    date_to_predict = st.date_input('Please enter a date', value=datetime(2022,12,2))# min_value=today, max_value=max_day)
     bt1 = st.form_submit_button('Make prediction')
 
 date_to_pick = date_to_predict.strftime("%Y-%m-%d")
