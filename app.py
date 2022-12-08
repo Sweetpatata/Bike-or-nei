66d660391d6a486c49ffb74366684b077c89b999
Frederick Johannson <f.johannson@hotmail.de>
2022-12-08 09:12:41 +0100

    cosmetic changes to the map

--- app.py
+++ app.py
@@ -45,7 +45,8 @@ td = timedelta(days=5)
 max_day = today + td
 
 with st.form(key='params_for_api'):
-    date_to_predict = st.date_input('Please enter a date', value=datetime(2022,12,2))# min_value=today, max_value=max_day)
+    date_title = st.markdown('<p style="color:Back;">Please enter a date</p>')
+    date_to_predict = st.date_input(date_title, value=datetime(2022,12,2))# min_value=today, max_value=max_day)
     bt1 = st.form_submit_button('Make prediction')
 
 date_to_pick = date_to_predict.strftime("%Y-%m-%d")
