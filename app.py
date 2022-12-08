a91bd3bfb8170e54676f7a382210225bbdbec287
Frederick Johannson <f.johannson@hotmail.de>
2022-12-08 09:22:44 +0100

    cosmetic changes to the map

--- app.py
+++ app.py
@@ -43,7 +43,8 @@ max_day = today + td
 
 
 with st.form(key='params_for_api'):
-    ''' ### Please choose date '''
+    select_title = '<p style="color:Black; font-size: 30px;">Please choose date</p>'
+    st.markdown = (select_title)
     date_to_predict = st.date_input('Please enter a date', value=datetime(2022,12,2), label_visibility='hidden')# min_value=today, max_value=max_day)
     bt1 = st.form_submit_button('Make prediction')
 
